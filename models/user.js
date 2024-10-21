e48bc1417b2d120a5dce1a2f0ae0de810467c79b
sk-yates <syates.edu@gmail.com>
2024-10-21 20:55:03 +0100

    Home page: reset

--- models/user.js
+++ models/user.js
@@ -1,14 +1,21 @@
+const { application } = require('express');
 const mongoose = require('mongoose');
 
+const applicationSchema = new mongoose.Schema({
+  company:     { type: String, required: true },
+  title:       { type: String, required: true },
+  notes:       { type: String },
+  postingLink: { type: String },
+  status: {
+    type: String,
+    enum: ["intrested", "applied", "interviewing", "accepted", "rejected"],
+  }
+});
+
 const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  username: { type: String, required: true, },
+  password: { type: String, required: true, },
+  applications: [applicationSchema]
 });
 
 const User = mongoose.model('User', userSchema);
